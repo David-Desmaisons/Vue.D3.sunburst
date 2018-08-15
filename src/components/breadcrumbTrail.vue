b639d26efea9dbeee68f709e2e5d69db41dde762
desmaisons_david <desmaisons_david@hotmail.com>
2018-08-15 00:28:15 -0300

    Improvement breadcrumbTrail.vue

--- src/components/breadcrumbTrail.vue
+++ src/components/breadcrumbTrail.vue
@@ -74,6 +74,65 @@ export default {
         points.push(this.tailWidth + "," + this.itemHeight / 2);
       }
       return points.join(" ");
+    },
+    draw() {
+      if (!this.current) {
+        select(this.$el)
+          .select(".trail")
+          .style("visibility", "hidden");
+        return;
+      }
+      const nodeArray = this.current.ancestors().reverse();
+
+      // Data join; key function combines name and depth (= position in sequence).
+      var trail = select(this.$el)
+        .select(".trail")
+        .selectAll("g")
+        .data(nodeArray, d => d.data.name + d.depth);
+
+      // Remove exiting nodes.
+      trail.exit().remove();
+
+      // Add breadcrumb and label for entering nodes.
+      var entering = trail.enter().append("svg:g");
+
+      entering
+        .append("svg:polygon")
+        .attr("points", this.breadcrumbPoints)
+        .style("fill", this.colorGetter);
+
+      entering
+        .append("svg:text")
+        .attr("x", (this.itemWidth + this.tailWidth) / 2)
+        .attr("y", this.itemHeight / 2)
+        .attr("dy", "0.25em")
+        .attr("text-anchor", "middle")
+        .text(d => d.data.name);
+
+      // Merge enter and update selections; set position for all nodes.
+      entering
+        .merge(trail)
+        .attr(
+          "transform",
+          (d, i) => "translate(" + i * (this.itemWidth + this.spacing) + ", 0)"
+        );
+
+      const percentage = 100 * this.current.value / this.root.value;
+      const text = `${percentage.toPrecision(3)} %`;
+
+      // Now move and update the percentage at the end.
+      select(this.$el)
+        .select(".trail")
+        .select(".endlabel")
+        .attr("x", (nodeArray.length + 0.5) * (this.itemWidth + this.spacing))
+        .attr("y", this.itemHeight / 2)
+        .attr("dy", "0.35em")
+        .attr("text-anchor", "middle")
+        .text(text);
+
+      select(this.$el)
+        .select(".trail")
+        .style("visibility", "");
     }
   },
   watch: {
@@ -84,67 +143,14 @@ export default {
     },
     current: {
       deep: true,
-      handler(current) {
-        if (!current) {
-          select(this.$el)
-            .select(".trail")
-            .style("visibility", "hidden");
-          return;
-        }
-        const nodeArray = current.ancestors().reverse();
-
-        // Data join; key function combines name and depth (= position in sequence).
-        var trail = select(this.$el)
-          .select(".trail")
-          .selectAll("g")
-          .data(nodeArray, d => d.data.name + d.depth);
-
-        // Remove exiting nodes.
-        trail.exit().remove();
-
-        // Add breadcrumb and label for entering nodes.
-        var entering = trail.enter().append("svg:g");
-
-        entering
-          .append("svg:polygon")
-          .attr("points", this.breadcrumbPoints)
-          .style("fill", this.colorGetter);
-
-        entering
-          .append("svg:text")
-          .attr("x", (this.itemWidth + this.tailWidth) / 2)
-          .attr("y", this.itemHeight / 2)
-          .attr("dy", "0.25em")
-          .attr("text-anchor", "middle")
-          .text(d => d.data.name);
-
-        // Merge enter and update selections; set position for all nodes.
-        entering
-          .merge(trail)
-          .attr(
-            "transform",
-            (d, i) =>
-              "translate(" + i * (this.itemWidth + this.spacing) + ", 0)"
-          );
-
-        const text = `${(
-          (100 * this.current.value) /
-          this.root.value
-        ).toPrecision(3)} %`;
-
-        // Now move and update the percentage at the end.
-        select(this.$el)
-          .select(".trail")
-          .select(".endlabel")
-          .attr("x", (nodeArray.length + 0.5) * (this.itemWidth + this.spacing))
-          .attr("y", this.itemHeight / 2)
-          .attr("dy", "0.35em")
-          .attr("text-anchor", "middle")
-          .text(text);
-
-        select(this.$el)
-          .select(".trail")
-          .style("visibility", "");
+      handler() {
+        this.draw();
+      }
+    },
+    root: {
+      deep: true,
+      handler() {
+        this.draw();
       }
     }
   }
