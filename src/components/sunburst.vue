57e2857e00aa7d8fb1e53f15f47bf7bb12864331
David Desmaisons <desmaisons_david@hotmail.com>
2021-06-06 16:24:18 -0300

    Clean-up

--- src/components/sunburst.vue
+++ src/components/sunburst.vue
@@ -564,8 +564,7 @@ export default {
           if (d.textAngle <= 180) {
             return false;
           }
-          const self = select(this);
-          const textLength = self.node().getComputedTextLength();
+          const textLength = select(this).node().getComputedTextLength();
           const maxLength = scaleY(d.y1) - scaleY(d.y0) + maxLabelText;
           if (textLength + 5 <= maxLength) {
             return false;
